77478bc9385deb7b757183646e43962022fa6237
SubcomandantePT <SubcomandantePT@users.noreply.github.com>
2017-06-20 20:01:24 -0700

    fixed jquery version conflict

--- Reddit_Post_Cheka.meta.js
+++ Reddit_Post_Cheka.meta.js
@@ -4,7 +4,6 @@
 // @include     https://www.reddit.com/*
 // @updateURL	https://github.com/SubcomandantePT/post-cheka-js/raw/master/Reddit_Post_Cheka.meta.js
 // @downloadURL	https://github.com/SubcomandantePT/post-cheka-js/raw/master/Reddit_Post_Cheka.user.js
-// @version     1.0.0
+// @version     1.0.1
 // @grant       none
-// @require		https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
